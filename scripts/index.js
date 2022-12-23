b097cebb258ba8b9d8ff1c1e591658893bc7522c
Asenyka <humsterchik@yandex.ru>
2022-12-23 11:22:52 +0300

    fix: implement comments_v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,31 @@
 let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
-let nameInput = document.querySelector("#name");
-let jobInput = document.querySelector("#job");
+let nameInput = document.querySelector(".popop__input_box_name");
+let jobInput = document.querySelector(".popup__input_box_job");
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
-
+let closeButton = document.querySelector(".popup__close-button");
+let formElement = document.querySelector(".popup__form");
 function popupShow(event) {
-  popup.setAttribute("style", "display: flex;");
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.innerHTML;
   jobInput.value = profileJob.innerHTML;
 }
 editButton.addEventListener("click", popupShow);
 
-let closeButton = document.querySelector(".popup__close-button");
+
 function popupHide(event) {
-  popup.setAttribute("style", "display: none;");
+  popup.classList.remove('popup_opened');
 }
 closeButton.addEventListener("click", popupHide);
 
-let formElement = document.querySelector(".popup__form");
+
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  popupHide();
 }
 
-formElement.addEventListener("submit", handleFormSubmit);
+formElement.addEventListener("submit", handleFormSubmit, popupHide);
