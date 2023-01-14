caded6206eb0295fc7e8b71d191d9db501bd617d
Asenyka <humsterchik@yandex.ru>
2023-01-14 18:16:35 +0300

    feat:render initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__input_box_name');
-let jobInput = document.querySelector('.popup__input_box_job');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let closeButton = document.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__form');
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const nameInput = document.querySelector('.popup__input_box_name');
+const jobInput = document.querySelector('.popup__input_box_job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const closeButton = document.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__form');
+
 function popupShow(event) {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.innerHTML;
@@ -26,3 +27,68 @@ editButton.addEventListener('click', popupShow);
 closeButton.addEventListener('click', popupHide);
 
 formElement.addEventListener('submit', handleFormSubmit, popupHide);
+
+
+const initialCards = [
+  {
+    name: 'Бермамыт',
+    link: './images/bermamyt.jpg'
+  },
+  {
+    name: 'Дивногорск',
+    link: './images/divnogorsk.jpg'
+  },
+  {
+    name: 'Эльтон',
+    link: './images/elton.jpg'
+  },
+  {
+    name: 'Хибины',
+    link: './images/khibiny.jpg'
+  },
+  {
+    name: 'Рускеала',
+    link: './images/ruskeala.jpg'
+  },
+  {
+    name: 'Усьвинские столбы',
+    link: './images/usyvinskie-stolby.jpg'
+  }
+]; 
+
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document
+	.querySelector('.places__template')
+	.content
+	.querySelector('.places__item');
+
+
+
+function cloneCard(event) {
+	const clonedCard = event.target.closest('.places__item').cloneNode(true);
+	addCard(clonedCard);
+}
+
+
+function createCard(newCard) {
+	const card = cardTemplate.cloneNode(true);
+	const cardText = card.querySelector('.places__title');
+  const cardImg = card.querySelector('.places__image');
+	cardText.textContent = newCard.name;
+  cardImg.src = newCard.link;  
+
+	return card;
+}
+
+function addCard(card) {
+	cardsContainer.prepend(card);
+}
+
+function renderCards(texts) {
+	texts.reverse().forEach(item => {
+		const cardHtml = createCard(item);
+		addCard(cardHtml);
+	});
+}
+
+renderCards(initialCards);
