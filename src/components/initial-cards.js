9e0a131349338111dd3e09fb021c79f351544078
Asenyka <humsterchik@yandex.ru>
2023-03-16 20:59:42 +0300

    final

---
+++ src/components/initial-cards.js
@@ -0,0 +1,34 @@
+import bermamyt from '../images/bermamyt.jpg'
+import divnogorsk from '../images/divnogorsk.jpg'
+import elton from '../images/elton.jpg'
+import khibiny from '../images/khibiny.jpg'
+import ruskeala from '../images/ruskeala.jpg'
+import usyvinskieStolby from '../images/usyvinskie-stolby.jpg'
+const initialCards = [
+  {
+    name: 'Бермамыт',
+    link: bermamyt,
+  },
+  {
+    name: 'Дивногорск',
+    link: divnogorsk,
+  },
+  {
+    name: 'Эльтон',
+    link: elton,
+  },
+  {
+    name: 'Хибины',
+    link: khibiny,
+  },
+  {
+    name: 'Рускеала',
+    link: ruskeala,
+  },
+  {
+    name: 'Усьвинские столбы',
+    link: usyvinskieStolby,
+  },
+];
+
+export default initialCards
